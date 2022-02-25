fdb3b5a972a699a3814e6a0b6f88967ca679528d
rdiazca <rdiazca96@gmail.com>
2022-02-24 23:08:50 -0500

    Formulario

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './bootstrap.min.css';
 import Header from './components/Header';
+import NuevaCita from './components/NuevaCita';
 
 class App extends Component {
   state = {
@@ -8,10 +9,16 @@ class App extends Component {
    }
   render(){
     return ( 
-      <div class="container">
+      <div className="container">
           <Header
             titulo="Administrador Pacientes Veterinaria"
           />
+
+          <div className="row">
+              <div className="col-md-10 mx-auto">
+              <NuevaCita />
+              </div>
+          </div>
       </div>
     );
   }
