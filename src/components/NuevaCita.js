2ac9f9deb37b4c2e6fa5e4ba4069ca5ab1aa56dc
rdiazca <rdiazca96@gmail.com>
2022-03-08 07:13:58 -0500

    Mostrar mensaje de error

--- src/components/NuevaCita.js
+++ src/components/NuevaCita.js
@@ -57,12 +57,17 @@ class NuevaCita extends Component {
 
     }
     render() {
+
+        //extraer valor de state
+        const { error } = this.state;
         return (
             <div className='card mt-5 py-5'>
                 <div className='card-body'>
                     <h2 className='card-title text-center mb-5'>
                         Llena el formulario para crear una nueva cita
                     </h2>
+                    { error ? <div className='alert alert-danger mt-2 mb-5 text-center'>Todos los campos son obligatorios</div> : null }
+                    
                     <form
                         onSubmit={this.handleSubmit}
                     >
