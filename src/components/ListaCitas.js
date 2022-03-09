6d42283f3e810fb8d0880307e941cf06af477c48
rdiazca <rdiazca96@gmail.com>
2022-03-09 15:57:51 -0500

    Mostrar mensaje condicional
    Mostrar mensaje condicional

--- src/components/ListaCitas.js
+++ src/components/ListaCitas.js
@@ -2,22 +2,29 @@ import React from 'react'
 import Cita from './Cita';
 import PropTypes from 'prop-types';
 
-const ListaCitas = ({citas, eliminarCita}) =>  (
-       <div className='card mt-2 py-5'>
-           <div className='card-body'>
-               <h2 className='card-title text-center'> Administra las citas aquí</h2>
-               <div className='lista-citas'>
-                    {citas.map(cita => (
-                        <Cita 
-                            key = {cita.id}
-                            cita = {cita}
-                            eliminarCita = {eliminarCita}
-                        />
-                    ))}
-               </div>
-           </div>
-       </div>
-    );
+const ListaCitas = ({citas, eliminarCita}) =>  {
+
+    // imprimir mensaje en base a si hay citas o no
+    const mensaje = Object.keys(citas).length === 0 ? 'No hay citas' : 'Administra las citas aquí';
+
+    return (
+        <div className='card mt-2 py-5'>
+            <div className='card-body'>
+                <h2 className='card-title text-center'>{mensaje}</h2>
+                <div className='lista-citas'>
+                     {citas.map(cita => (
+                         <Cita 
+                             key = {cita.id}
+                             cita = {cita}
+                             eliminarCita = {eliminarCita}
+                         />
+                     ))}
+                </div>
+            </div>
+        </div>
+     );
+}
+
 
     ListaCitas.propTypes = {
         citas : PropTypes.array.isRequired,
