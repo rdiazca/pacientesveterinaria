d5b1e17c8398055423352766efa9ed4ececf98a6
rdiazca <rdiazca96@gmail.com>
2022-03-09 12:03:35 -0500

    Eliminar cita

--- src/App.js
+++ src/App.js
@@ -19,6 +19,21 @@ class App extends Component {
        citas
       })
    }
+
+   //elimina las citas del state
+   eliminarCita = id => {
+     //tomar una copia del state
+     const citasActuales = [...this.state.citas];
+
+     //utilizar filter para sacar el elemento @id del arreglo
+     const citas = citasActuales.filter(cita => cita.id !== id)
+   
+     //actualizar el state
+     this.setState({
+       citas
+     })
+
+   }
   render(){
     return ( 
       <div className="container">
@@ -35,6 +50,7 @@ class App extends Component {
               <div className='mt-5 col-md-10 mx-auto'>
                   <ListaCitas 
                     citas = {this.state.citas}
+                    eliminarCita = {this.eliminarCita}
                   />
 
               </div>
--- src/components/Cita.js
+++ src/components/Cita.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ListaCitas from './ListaCitas';
 
-const Cita = ({cita}) => (
+const Cita = ({cita, eliminarCita}) => (
        <div className='media mt-3'>
            <div className='media-body'>
                <h3 className='mt-0'>{cita.mascota}</h3>
@@ -10,6 +10,11 @@ const Cita = ({cita}) => (
                <p className='card-text'><span>Hora:</span> {cita.hora}</p>
                <p className='card-text'><span>Síntomas:</span></p>
                <p className='card-text'>{cita.sintomas}</p>
+
+               <button 
+               className='btn btn-danger'
+               onClick={() => eliminarCita(cita.id)}
+               >Borrar &times;</button>
            </div>
        </div>
     );
